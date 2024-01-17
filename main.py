09120bc012ba555c7e38ded3b282e5c879d519f9
AG_iSpace <99149310+ParadoxPlays@users.noreply.github.com>
2024-01-16 20:54:09 -0600

    Update main.py
    Added more commands and multiple different prefixes.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+#main.py
 import disnake
 from disnake.ext import commands
+import random
 
-client = commands.Bot(command_prefix="!", case_insensitive=True)
+#Ensure you are not using a prefix that another bot has, if so it will be buggy.
+PREFIXES = ["!", "?", "-", ".", ","]
+
+client = commands.Bot(command_prefix=PREFIXES, case_insensitive=True)
 client.remove_command("help")
 
 @client.event
@@ -11,6 +16,38 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"Pong!, Bot Latency: {round(client.latency * 1000)}ms.")
+    await ctx.reply(f"Pong!, Bot Latency: {round(client.latency * 1000)}ms.")
 
+@client.command(aliases=["8b", "8ball", "eb"])
+async def eightball(ctx, *, question):
+    responses = ["It is certain.","It is decidedly so.","Without a doubt.","Yes - definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
+    await ctx.reply(f":8ball: Your Question: {question}\n:8ball: Answer: {random.choice(responses)}")
+
+@client.command()
+async def rps(ctx, choice: choices):
+    choices = ["rock", "paper", "scissors"]
+    await ctx.reply(f"Bot Choice: {random.choice(choices)}\nYour Choice: {choice}")
+
+@client.command()
+async def status(ctx, *, activity):
+    await client.change_presence(activity=disnake.Game(name=activity))
+    await ctx.reply(f"Status changed to {activity}.")
+
+@client.command()
+async def add(ctx, left: float, right: float):
+    await ctx.reply(f"Answer: {str(left + right)}")
+
+@client.command()
+async def subtract(ctx, left: float, right: float):
+    await ctx.reply(f"Answer: {str(left - right)}")
+
+@client.command()
+async def multiply(ctx, left: float, right: float):
+    await ctx.reply(f"Answer: {str(left * right)}")
+
+@client.command()
+async def divide(ctx, left: float, right: float):
+    await ctx.reply(f"Answer: {str(left / right)}")
+
+#Your token goes here.
 client.run("TOKEN")
